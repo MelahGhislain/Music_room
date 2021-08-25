0db657355e8f55fa385d18af7798b44bf45fd1e3
MelahGhislain <melahghislain17@gmail.com>
2021-08-25 11:48:25 +0100

    new project

---
+++ music_app/frontend/src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {render} from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoom from "./CreateRoom";
+import HomePage from './HomePage';
+
+export default function App(){
+    return (
+        <Router>
+        <Switch>
+          <Route exact path="/" component={ HomePage } />
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoom} />
+        </Switch>
+      </Router>
+    );
+}
+
+const appDiv = document.getElementById('app');
+
+render(<App />, appDiv);
