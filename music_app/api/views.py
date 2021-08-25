83f471fe82f1c1bef302df84a6e611907e68b6fd
MelahGhislain <melahghislain17@gmail.com>
2021-08-25 21:43:19 +0100

    created the create room view

--- music_app/api/views.py
+++ music_app/api/views.py
@@ -11,6 +11,32 @@ class RoomView(generics.ListAPIView):
     serializer_class = RoomSerializer
 
 class CreateRoomView(APIView):
-    serializer_class = CreateRoomSerialiser
+    # serializer_class = CreateRoomSerialiser
     def post(self, request):
-        pass
+        # create a session key if it doesn't exit
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+
+        serializer = CreateRoomSerialiser(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.validated_data['guest_can_pause']
+            vote_to_skip = serializer.validated_data['vote_to_skip']
+            # uses the session key as the host since session keys are unique
+            host = self.request.session.session_key
+            # verifies if the host has a room 
+            # if True -> update the room
+            # if False -> create a new room
+            queryset = Room.objects.filter(host=host)
+            if queryset.exist():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.vote_to_skip = vote_to_skip
+                room.save(update_fields=['guest_can_pause', 'vote_to_skip'])
+            else:
+                room = Room(host=host,guest_can_pause=guest_can_pause,vote_to_skip=vote_to_skip)
+                if room.is_valid():
+                    room.save()
+            return Response(room, status=status.HTTP_201_CREATED)
+
+ 
